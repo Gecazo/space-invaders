90b600b9bd60f74f5f3a2a43f68af984592fd957
Filip <43730405+Gecazo@users.noreply.github.com>
2021-09-01 18:46:21 +0300

    Continuous ship movement added

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,7 +21,9 @@ class AlienInvasion():
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
+            
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -29,8 +31,10 @@ class AlienInvasion():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
-                    
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
